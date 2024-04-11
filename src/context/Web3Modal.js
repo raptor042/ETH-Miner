1972ca455fcbac29f040601a929f6bd30acdfeb9
Benji Chimex <benjichimex@MacBooks-MacBook-Pro.local>
2024-04-11 12:38:18 +0100

    first commit

--- src/context/Web3Modal.js
+++ src/context/Web3Modal.js
@@ -4,37 +4,37 @@ import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react"
 
 const projectId = "a7784566359fedc53b9586ee543d8b35"
 
-// const mainnet = {
-//     chainId: 1,
-//     name: "Ethereum",
+const mainnet = {
+    chainId: 1,
+    name: "Ethereum",
+    currency: "ETH",
+    explorerUrl: "https://etherscan.io",
+    rpcUrl: "https://mainnet.infura.io/v3/0253203d40d344978948e4641ac65adb"
+}
+
+const sepolia = {
+    chainId: 11155111,
+    name: "Sepolia Testnet",
+    currency: "SepoliaETH",
+    explorerUrl: "https://sepolia.etherscan.io",
+    rpcUrl: "https://sepolia.infura.io/v3/0253203d40d344978948e4641ac65adb"
+}
+
+// const base_mainnet = {
+//     chainId: 8453,
+//     name: "Base Mainnet",
 //     currency: "ETH",
-//     explorerUrl: "https://etherscan.io",
-//     rpcUrl: "https://mainnet.infura.io/v3/0253203d40d344978948e4641ac65adb"
+//     explorerUrl: "https://basescan.org",
+//     rpcUrl: "https://mainnet.base.org"
 // }
 
-// const sepolia = {
-//     chainId: 11155111,
-//     name: "Sepolia Testnet",
-//     currency: "SepoliaETH",
-//     explorerUrl: "https://sepolia.etherscan.io",
-//     rpcUrl: "https://sepolia.infura.io/v3/0253203d40d344978948e4641ac65adb"
+// const base_sepolia = {
+//     chainId: 84532,
+//     name: "Base Sepolia",
+//     currency: "ETH",
+//     explorerUrl: "https://sepolia.basescan.org",
+//     rpcUrl: "https://sepolia.base.org"
 // }
-
-const base_mainnet = {
-    chainId: 8453,
-    name: "Base Mainnet",
-    currency: "ETH",
-    explorerUrl: "https://basescan.org",
-    rpcUrl: "https://mainnet.base.org"
-}
-
-const base_sepolia = {
-    chainId: 84532,
-    name: "Base Sepolia",
-    currency: "ETH",
-    explorerUrl: "https://sepolia.basescan.org",
-    rpcUrl: "https://sepolia.base.org"
-}
 
 const metadata = {
     name: "Profit-IQ - Ethereum Miner",
@@ -44,7 +44,7 @@ const metadata = {
 createWeb3Modal({
     ethersConfig: defaultConfig({ metadata }),
     projectId,
-    chains: [base_mainnet, base_sepolia]
+    chains: [mainnet, sepolia]
 })
 
 export function Web3ModalProvider({ children }) {
